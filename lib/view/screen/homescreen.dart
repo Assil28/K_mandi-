6797c9e1d9e01bf119e04242be3c2d3547b4742b
Assil Dkhil <assil.dkhil28@gmail.com>
2024-08-16 16:47:39 +0100

    Dialog Exit

--- lib/view/screen/homescreen.dart
+++ lib/view/screen/homescreen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -14,22 +16,53 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     Get.put(HomeScreenControllerImp());
 
-
     return GetBuilder<HomeScreenControllerImp>(
         builder: (controller) => Scaffold(
-          // l botton l jeya f west l bottomNavBar
-          floatingActionButton: FloatingActionButton(
-            backgroundColor: AppColor.primaryColor,
-              onPressed: () {
-                Get.toNamed(AppRoutes.cart);
-              },
-              child: const Icon(Icons.shopping_basket_outlined)),
-          floatingActionButtonLocation:
-          FloatingActionButtonLocation.centerDocked,
-          bottomNavigationBar: const CustomBottomAppBarHome(),
+              // l botton l jeya f west l bottomNavBar
+              floatingActionButton: FloatingActionButton(
+                  backgroundColor: AppColor.primaryColor,
+                  onPressed: () {
+                    Get.toNamed(AppRoutes.cart);
+                  },
+                  child: const Icon(Icons.shopping_basket_outlined)),
+              floatingActionButtonLocation:
+                  FloatingActionButtonLocation.centerDocked,
+              bottomNavigationBar: const CustomBottomAppBarHome(),
 
-          // hne 3ayetet lel les pages l bch naviger binethom   wel bottomNavBar tab9a fix
-          body: controller.listPage.elementAt(controller.currentpage),
-        ));
+              // // hne 3ayetet lel les pages l bch naviger binethom   wel bottomNavBar tab9a fix
+              // body: controller.listPage.elementAt(controller.currentpage),
+
+              // hne zidet fazet bech k nenzel ale flesh retour w m3ach fama win narja3 ma3neha lapplication b tokhrej ya3meli alert
+              body: WillPopScope(
+                  // hne 3ayetet lel les pages l bch naviger binethom   wel bottomNavBar tab9a fix
+                  child: controller.listPage.elementAt(controller.currentpage),
+                  onWillPop: () {
+                    Get.defaultDialog(
+                        title: "53".tr,
+                        middleText: "50".tr,
+                        titleStyle: const TextStyle(
+                            color: AppColor.primaryColor,
+                            fontWeight: FontWeight.bold),
+                        actions: [
+                          ElevatedButton(
+                              style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(
+                                      AppColor.primaryColor)),
+                              onPressed: () {
+                                exit(0);
+                              },
+                              child: Text("51".tr)),
+                          ElevatedButton(
+                              style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(
+                                      AppColor.primaryColor)),
+                              onPressed: () {
+                                Get.back();
+                              },
+                              child: Text("52".tr))
+                        ]);
+                    return Future.value(false);
+                  }),
+            ));
   }
 }
